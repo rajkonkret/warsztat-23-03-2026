7ba260ad5b9d82225620c25df380f56fd1a2b865
radek <rajkonkret660@gmail.com>
2026-03-24 10:37:47 +0100

    [2 5] update funkcja3.py

---
+++ day1/funkcje3.py
@@ -0,0 +1,11 @@
+# funkcja wyższego rzędu
+# funkcja, która przyjmuje inną funkcję jako argument
+
+def witaj(imie):
+    return f"Miło Cię widzieć {imie}"
+
+
+def konkurs(imie, miejsce, punkty):
+    return f"Uczestnik konkursu: {imie}, miejsce: {miejsce}, liczba punktów: {punkty}"
+
+
