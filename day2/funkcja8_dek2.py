2fafc6ede1aec21c63d8d9d57d95d74fe27d05cf
rjaniak <rajkonkret660@gmail.com>
2026-03-24 15:27:41 +0100

    [2 26] update fumkcja8_dek2

--- day2/funkcja8_dek2.py
+++ day2/funkcja8_dek2.py
@@ -32,3 +32,33 @@ def greeting2(string):
 
 
 print(greeting2("Test Radek"))
+
+
+# kolejnośc ma znaczenie
+@bold_decorator
+@uppercase_decorator
+def greeting3(string):
+    return f"(dwa) Podałeś: {string}"
+
+
+print(greeting3("test dwa"))
+
+# colorama
+from colorama import Fore, Style, init
+
+
+# pip install colorama
+
+def color_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return Fore.RED + result + Style.RESET_ALL
+
+    return wrapper
+
+
+@color_decorator
+def greeting4(string):
+    return f"(color) Podałeś: {string}"
+
+print(greeting4("Radek 8"))
