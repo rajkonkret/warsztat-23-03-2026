157e49a717f839cba2f817d6a3ec7d83b96a3759
rjaniak <rajkonkret660@gmail.com>
2026-03-24 15:06:02 +0100

    [2 23] update fumkcja7

---
+++ day2/funkcja8_dek2.py
@@ -0,0 +1,16 @@
+# zrobic dekorator, który zmienia wynik funkcji na duże litery
+
+# "Radek".upper()
+def uppercase_decorator(func):
+    def wew(*args, **kwargs):
+        return func(*args, **kwargs).upper()
+
+    return wew  # zwracamy adres funkcji, referencję
+
+
+@uppercase_decorator
+def greeting():
+    return "Hello world!"
+
+
+print(greeting())  # HELLO WORLD!
