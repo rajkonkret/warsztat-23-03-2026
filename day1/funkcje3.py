0096c5c5f3de804bbaa965214252e3087f4408bf
radek <rajkonkret660@gmail.com>
2026-03-24 11:39:47 +0100

    [2 10] update funkcja3.py

--- day1/funkcje3.py
+++ day1/funkcje3.py
@@ -35,7 +35,50 @@ print(osoba(konkurs, "Leon", "Szczebrzeszyn", 89))
 # Uczestnik konkursu: Leon, miejsce: Szczebrzeszyn, liczba punktów: 89
 
 print(multiosoba("Anna", "Toruń", 90))
+
+
 # Liczba punktów z bonusem: 100
 # Imie: Anna
 # Uczestnik konkursu: Anna, miejsce: Toruń, liczba punktów: 90
 # opublikowano wyniki konkursu
+
+def fun_args(*args):  # dowolna ilość argumentó pozycyjnych
+    print(args)
+    print(type(args))
+
+
+fun_args()  # ()
+fun_args(1, 2, 3)  # (1, 2, 3)
+fun_args(1, 2, 3, 4, 5, 6, 7, 8)  # (1, 2, 3, 4, 5, 6, 7, 8)
+
+
+# <class 'tuple'>
+
+def fun_kwargs(**kwargs):  # dowolna ilośc argumentów nazwanych
+    print(kwargs)
+    print(type(kwargs))
+
+
+fun_kwargs()
+fun_kwargs(a=10)
+fun_kwargs(a=10, b=56, name="Radek")
+
+
+# <class 'dict'>
+
+
+def all_args(*args, **kwargs):
+    print(args, kwargs)
+
+
+all_args()
+all_args(1, 2, 3)
+all_args(a=10)
+all_args(1, 2, a=10)
+# () {}
+# (1, 2, 3) {}
+# () {'a': 10}
+# (1, 2) {'a': 10}
+
+# SyntaxError: positional argument follows keyword argument
+# all_args(a=10, 1, 2)
