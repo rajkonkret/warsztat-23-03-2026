c176530985a771a24aaff5c914dc001b16c97a43
rjaniak <rajkonkret660@gmail.com>
2026-03-25 14:41:47 +0100

    [3 32] update klasy

--- day3/kl1.py
+++ day3/kl1.py
@@ -21,8 +21,22 @@ class MyFirstClass:
         :param y:
         """
         # self - obiekt przekazany do klasy
+        # self.x = x
+        # self.y = y
+        self.move(x,y)
+
+    def move(self, x: int, y: int) -> None:
+        """
+        zmienia x i y obiektu na nowe wartości
+        :param x:
+        :param y:
+        :return:
+        """
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f"{self.x, self.y}"
 
 
 ob = MyFirstClass()
@@ -32,3 +46,9 @@ print(MyFirstClass.__doc__)  # klasa w Pythonie
 # pydoc -w .\kl1.py plik html z dokumentacją
 print(ob.x)
 print(ob.y)
+
+point1 = MyFirstClass(5, 9)
+print(point1.x)
+print(point1.y)
+print(point1)  # (5, 9) po napisaniu metody __str__
+
