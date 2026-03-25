7d6a43273b9b383640b889dbab3d4d560251560a
rjaniak <rajkonkret660@gmail.com>
2026-03-25 13:49:04 +0100

    [3 28] update klasy

--- day3/kl1.py
+++ day3/kl1.py
@@ -5,3 +5,18 @@
 # klasa musi byc najpierw zadeklarowana
 # tworzenie obiektu uruchamia metode inicjalizującą (__init__) - konstruktor
 # hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+import math
+
+
+# PascalCase, UpperCamelCase
+class MyFirstClass:
+    """
+    klasa w Pythonie
+    """
+
+
+ob = MyFirstClass()
+print(ob)  # <__main__.MyFirstClass object at 0x000002083AB90C20>
+print(MyFirstClass.__doc__)  # klasa w Pythonie
+# pydoc -b     serwer dokumentacji
+# pydoc -w .\kl1.py plik html z dokumentacją
