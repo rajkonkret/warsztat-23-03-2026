69e47fad4b66eb1c43fcf8de669afd3f087afe09
rjaniak <rajkonkret660@gmail.com>
2026-03-25 13:03:12 +0100

    [3 22] update transakcje.py

--- day3/transakcje.py
+++ day3/transakcje.py
@@ -41,4 +41,33 @@ def reduce_transactions(mapped):
 
 
 def process_transactions(transactions, transaction_type, currency):
-    pass
+    filtered = filter_transactions(transactions, transaction_type)
+    mapped = map_transactions(filtered, currency)
+    total = reduce_transactions(mapped)
+
+    return total
+
+
+def test_transactions_processing():
+    assert (map_transactions(filter_transactions(transactions, "income"), 'USD')
+            == [1000, 500, 700, 0])
+
+
+if __name__ == '__main__':
+    print(process_transactions(transactions, "expense", "EUR"))  # 400
+
+    # cd .\day3\    wejscie do katlaogu
+    # cd .. wyjscie katalog wyżej
+    # pip install pytest narzedzie do uruchamiania testow
+    # transakcje.py::test_transactions_processing PASSED
+    # pytest -v .\transakcje.py  bardziej szczegółówa informacja
+    # ======================================================================================================= test session starts =======================================================================================================
+    # platform win32 -- Python 3.14.0, pytest-9.0.2, pluggy-1.6.0 -- C:\Users\cscomarch\PycharmProjects\warsztat-23-03-2026\.venv\Scripts\python.exe
+    # cachedir: .pytest_cache
+    # rootdir: C:\Users\cscomarch\PycharmProjects\warsztat-23-03-2026\day3
+    # collected 1 item
+    #
+    # transakcje.py::test_transactions_processing PASSED                                                                                                                                                                           [100%]
+    #
+    # ======================================================================================================== 1 passed in 0.03s ========================================================================================================
+    # (.venv) PS C:\Users\cscomarch\PycharmProjects\warsztat-23-03-2026\day3>
