cdcdb7e7e0d542cb3426ff3daeeb2199439b2a03
rjaniak <rajkonkret660@gmail.com>
2026-03-25 13:42:59 +0100

    [3 27] update klasy

---
+++ day3/kl1.py
@@ -0,0 +1,7 @@
+# klasy - element programowanie obiektowego
+# szablon, przepis
+# zawiery cechy(zmienne) i metody (funkcje)
+# obiekt klasy (instancja) - zbudowany wg przepisu
+# klasa musi byc najpierw zadeklarowana
+# tworzenie obiektu uruchamia metode inicjalizującą (__init__) - konstruktor
+# hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
