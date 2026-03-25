e9fd30a1eb9962404df29a1695c7d49786af3810
rjaniak <rajkonkret660@gmail.com>
2026-03-25 09:56:41 +0100

    [3 5] update fumkcja11_gen2

--- day3/funkcja11_gen2.py
+++ day3/funkcja11_gen2.py
@@ -35,9 +35,9 @@ print(list(wznowienie(6, 7)))
 # [1001, 13, 11, 126, 2.5714285714285716]
 
 print(20 * "-")
-for i in wznowienie(6,8):
+for i in wznowienie(6, 8):
     if i == 1001:
-        continue # pomin eleemnt, wstrzymuje iteracje pętli i bierzrze kolejny element
+        continue  # pomin eleemnt, wstrzymuje iteracje pętli i bierzrze kolejny element
     time.sleep(1)
     print(f"Yield zwraca wartosc: {i}")
 # --------------------
@@ -54,3 +54,67 @@ for i in wznowienie(6,8):
 
 print(20 * "-")
 
+
+def gen4():
+    i = 1
+    while True:
+        yield i * i
+        i += 1
+
+
+generator4 = gen4()
+print(next(generator4))
+print(next(generator4))
+print(next(generator4))
+print(next(generator4))
+
+
+# --------------------
+# 1
+# 4
+# 9
+# 16
+
+def gen5():
+    i = 1
+    while True:
+        command = yield i * i
+        print(command)
+        if command == 'OK':
+            print("Jestem generatorem")
+        elif command == "stop":
+            break
+        i += 1
+
+
+g5 = gen5()
+print(next(g5))
+print(next(g5))
+print(next(g5))
+print(next(g5))
+# 1
+# None
+# 4
+# None
+# 9
+# None
+# 16
+
+g5.send("OK")
+# 16
+# OK
+# 16
+# OK
+# Jestem generatorem
+print(next(g5))
+# None
+# 36
+try:
+    g5.send("stop")
+except StopIteration:
+    print("Koniec generatora")
+except Exception as e:
+    print("błąd:", e)
+# stop
+# Koniec generatora
+g5.close() # zatrzymanie generatora i wskazanie do gc aby usunął
