cf98357fc28aafe97ab8ae5a874dcf70863dbefa
rjaniak <rajkonkret660@gmail.com>
2026-03-25 14:34:41 +0100

    [3 31] update klasy

--- day3/kl1.py
+++ day3/kl1.py
@@ -14,6 +14,16 @@ class MyFirstClass:
     klasa w Pythonie
     """
 
+    def __init__(self, x=0, y=0):
+        """
+        Metoda inicjalizująca (konstruktor)
+        :param x:
+        :param y:
+        """
+        # self - obiekt przekazany do klasy
+        self.x = x
+        self.y = y
+
 
 ob = MyFirstClass()
 print(ob)  # <__main__.MyFirstClass object at 0x000002083AB90C20>
@@ -21,3 +31,4 @@ print(MyFirstClass.__doc__)  # klasa w Pythonie
 # pydoc -b     serwer dokumentacji
 # pydoc -w .\kl1.py plik html z dokumentacją
 print(ob.x)
+print(ob.y)
