53910281af716b95d749104b7793d8accf786685
rjaniak <rajkonkret660@gmail.com>
2026-03-26 09:37:05 +0100

    [4 4] update klasy4

--- day4/kl4.py
+++ day4/kl4.py
@@ -30,4 +30,21 @@ print(a1['name'])  # name
 print(a1)  # {'name': 0}
 print(a1['name'])  # 0
 
+
 # zmienia klucze na małe litera
+class CaseInsensitiveDict(dict):
+    def __missing__(self, key):
+        # return self.get(key.lower())
+        if isinstance(key, str):  # sprawdzenie czy klucz jest str
+            return self.get(key.lower())
+        return key
+
+
+c1 = CaseInsensitiveDict()
+c1['name'] = 'Radek'
+print(c1)  # {'name': 'Radek'}
+print(c1['Name'])  # Radek
+print(c1['age'])  # None
+
+# c1[1] = 10
+print(c1[1])  # AttributeError: 'int' object has no attribute 'lower'
