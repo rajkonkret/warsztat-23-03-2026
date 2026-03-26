bb72e071eb00f12321835169400654047ffb2aab
rjaniak <rajkonkret660@gmail.com>
2026-03-26 12:30:08 +0100

    [4 23] update baza1

--- day4/baza/baza1.py
+++ day4/baza/baza1.py
@@ -5,8 +5,10 @@
 import sqlite3
 
 # docker
+# dbeaver
+# tablePlus
 try:
-    sql_connection = sqlite3.connect('sqlite_db.db')
+    sql_connection = sqlite3.connect('sqlite_db.db')  # nazwa pliku
     cursor = sql_connection.cursor()
     print("Baza danych została podłączona")
 except sqlite3.Error as e:
