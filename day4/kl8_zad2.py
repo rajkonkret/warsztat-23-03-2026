d1ba3e11663a6586dc880095fa492144256d2b98
rjaniak <rajkonkret660@gmail.com>
2026-03-26 10:29:54 +0100

    [4 14] update klasy zadania

---
+++ day4/kl8_zad2.py
@@ -0,0 +1,8 @@
+# Stworzyc system zarządzania biblioteką
+# Book -> title, author, isbn
+# dodanie ksiązki, wypozyczenie ksiązki, zwrot ksiązki
+# lista dostępnych
+# lista wypozyczonych
+# dodac klasę Library
+# dodac User
+# obsłużyć błędy
