e675f2342de54b7ae8909197c2ab62b6fee2c656
rjaniak <rajkonkret660@gmail.com>
2026-03-26 10:05:48 +0100

    [4 8] update klasy5

--- day4/kl6.py
+++ day4/kl6.py
@@ -22,3 +22,30 @@ class B:
 
 b = B()
 b.method()  # Metoda z klasy B
+
+
+# dziedziczenie po wielu klasach
+class C(B, A):
+    """
+    Klasa dziedziczy po klasie B i A
+    """
+
+
+c = C()
+c.method()  # Metoda z klasy B
+print(C.__mro__)
+
+
+# (<class '__main__.C'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
+
+class D(A, B):
+    """
+    Klasa dziedziczy po klasie A i B
+    """
+
+
+d = D()
+d.method()  # Metoda z klasy A
+print(D.__mro__)
+# (<class '__main__.D'>, <class '__main__.A'>, <class '__main__.B'>, <class 'object'>)
+
