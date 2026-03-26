32fb0d1fc151ca6b70845a0f4a720f343ad0f925
rjaniak <rajkonkret660@gmail.com>
2026-03-26 09:58:19 +0100

    [4 7] update klasy5

---
+++ day4/kl6.py
@@ -0,0 +1,24 @@
+class A:
+    """
+    Klasa A
+    """
+
+    def method(self):
+        print("Metoda z klasy A")
+
+
+a = A()
+a.method()  # Metoda z klasy A
+
+
+class B:
+    """
+    Klasa B
+    """
+
+    def method(self):
+        print("Metoda z klasy B")
+
+
+b = B()
+b.method()  # Metoda z klasy B
