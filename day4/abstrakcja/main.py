fc7b242b3370f5d9146f67759613f91458c0918f
rjaniak <rajkonkret660@gmail.com>
2026-03-26 11:49:13 +0100

    [4 19] update abstrakcja

---
+++ day4/abstrakcja/main.py
@@ -0,0 +1,9 @@
+from xyz import XYZ
+
+xyz1 = XYZ(1, 2, 3)
+print(xyz1.info("xyz 001"))
+print(f"Policz: {xyz1.policz()}")
+# xyz 001xyz 001xyz 001
+# Policz: 5
+xyz1.msg()
+# Metoda nieabstrakcyjna klasy abstrakcyjnej
