1804d526072d82c188de5fdb563c89a00c653dd8
rjaniak <rajkonkret660@gmail.com>
2026-03-26 09:54:06 +0100

    [4 6] update klasy5

--- day4/kl5.py
+++ day4/kl5.py
@@ -14,3 +14,18 @@ art['tomasz'] = 12
 art['abraham'] = 8
 art['zen'] = 2
 print(art.longest_key())  # abraham
+
+
+class LongestKeyDictMax(dict):
+    def longest_key(self):
+        return max(self.keys(), key=len, default=None)
+
+
+art = LongestKeyDictMax()
+art['tomasz'] = 12
+art['abraham'] = 8
+art['zen'] = 2
+print(art.longest_key())  # abraham
+
+assert "abraham" == art.longest_key()
+# assert "zen" == art.longest_key()  # AssertionError
