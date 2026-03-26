978518dda99be5e90f0a7047b8789f44fa7fa42e
rjaniak <rajkonkret660@gmail.com>
2026-03-26 14:51:51 +0100

    [4 34] update statyczne

---
+++ day4/metoda_statyczna_zad1.py
@@ -0,0 +1,29 @@
+# metoda statyczna - nie potrzebuje klasy
+
+
+class Matematyka:
+
+    # def dodaj(self, a, b):
+    #     return a + b
+
+    @staticmethod
+    def dodaj(a, b):
+        return a + b
+
+
+mat = Matematyka()
+print(mat.dodaj(4, 5))  # 9
+
+# metoda statyczna nie potrzebuje obiektu
+print(Matematyka.dodaj(6, 9))  # 15\
+zmienna = Matematyka.dodaj(7, 90)
+print(zmienna)  # 97
+print(type(zmienna))  # <class 'int'>
+
+
+# starsze podejscie
+class Kalkulator:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
